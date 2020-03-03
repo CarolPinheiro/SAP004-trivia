f54527e15b6c6cbdf4b17faafd0f321ce3ce45f5
CarolPinheiro <61391061+CarolPinheiro@users.noreply.github.com>
2020-03-03 17:32:12 -0300

    Update main.js
    Adicionado um segundo questionário

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ let desejaJogar = prompt("Deseja jogar? \n Digite número correspondente a sua e
 
 if (desejaJogar == 1) {
 
+    let tema = parseInt(prompt("Você deseja realizar um questionário sobre qual tema? \n Digite o número correspondente a sua escolha: \n 1. Matemática \n 2.HTML"));
+    
+    if (tema === 1){
+
     let primeiraPergunta = parseInt(prompt("Primeira Pergunda: \n Quanto é 2+2? \n Digite número correspondente a sua escolha: \n 1. 4 \n 2. 6 \n 3. 0"));
     if (primeiraPergunta === 1){
         let respostaCorreta1 = document.getElementById("resposta-correta-1");
@@ -41,7 +45,42 @@ if (desejaJogar == 1) {
          let respostaErrada3 = document.getElementById("resposta-errada-1");
         respostaErrada3.innerHTML += "<p>Você errou a questão 3</p>";
     }
+}
+else {
+    let questao1 = parseInt(prompt("Primeira Pergunda: \n O que é Header? \n Digite número correspondente a sua escolha: \n 1. Cabeçalho \n 2. Corpo \n 3. Comida "));
+    if (questao1 === 1){
+        let respostaCorreta1 = document.getElementById("resposta-correta-1");
+        respostaCorreta1.innerHTML += "<p>Você acertou a questão 1</p>";
+        
+    } else {
+         let respostaErrada1 = document.getElementById("resposta-errada-1");
+        respostaErrada1.innerHTML += "<p>Você errou a questão 1</p>";
+    }
+
+    let questao2 = parseInt(prompt("Segunda Pergunda: \n O que é uma div? \n Digite número correspondente a sua escolha: \n 1. Uma tag de divisão \n 2. Discussão \n 3. Dividendo"));
+    if (questao2 === 1){
+        let respostaCorreta2 = document.getElementById("resposta-correta-1");
+        respostaCorreta2.innerHTML +=  "<p>Você acertou a questão 2</p>";
+        
+    } else {
+         let respostaErrada2 = document.getElementById("resposta-errada-1");
+        respostaErrada2.innerHTML += "<p>Você errou a questão 2</p>";
+    }
      
+    let questao3 = parseInt(prompt("Terceira Pergunda: \n Pra que serve \\n ? \n Digite número correspondente a sua escolha: \n 1. Pular linha \n 2. Pular resposta \n 3. Parar"));
+    if (questao3 === 1){
+        let respostaCorreta3 = document.getElementById("resposta-correta-1");
+        respostaCorreta3.innerHTML += "<p>Você acertou a questão 3</p>";
+        
+    } else {
+         let respostaErrada3 = document.getElementById("resposta-errada-1");
+        respostaErrada3.innerHTML += "<p>Você errou a questão 3</p>";
+    }
+
+
+
+
+}
 
 } else {
 
